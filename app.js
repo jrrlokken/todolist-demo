79ad8b83e33ad50ff6ee9d33425a7a4cc332ce91
Joshua Lokken <jrrlokken@gmail.com>
2020-03-02 08:37:49 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// Check off specific todos by clicking
+$("ul").on("click", "li", function() {
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		var todoText = $(this).val();
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>");
+		$(this).val('');
+	}
+});
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+});
